6c5ed7ca5c423e2e806d632c3188c18bcf2b91b5
Grey Patterson <greypatterson@gmail.com>
2016-04-29 14:52:49 -0700

    Break homepage_post into its own partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
 		if !user_signed_in?
 			redirect_to new_user_registration_path, notice: "You need to be signed in for that."
 		end
+		@allFollows = current_user.all_follows
+		@arrayOfIds = @allFollows.map(&:followable_id)
+		@posts = Post.where(user_id: @arrayOfIds)
 	end
 end
