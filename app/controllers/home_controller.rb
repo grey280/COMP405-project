556ebf72db337b065030fbc9be6ee43c52c3f0f7
Grey Patterson <greypatterson@gmail.com>
2016-04-29 22:31:36 -0700

    Home_controller can deal with errors a little bit better now. I think.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,10 @@ class HomeController < ApplicationController
 	def index
 		if !user_signed_in?
 			redirect_to new_user_registration_path, notice: "You need to be signed in for that."
+		else 
+			@allFollows = current_user.all_follows
+			@arrayOfIds = @allFollows.map(&:followable_id)
+			@posts = Post.where(user_id: @arrayOfIds).order(created_at: :desc)
 		end
-		@allFollows = current_user.all_follows
-		@arrayOfIds = @allFollows.map(&:followable_id)
-		@posts = Post.where(user_id: @arrayOfIds)
 	end
 end
